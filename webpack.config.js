f8157c4be86813d2784e4486d0a00730c60e34c7
SuhairZain <suhairzain11@gmail.com>
2016-05-12 01:47:24 +0530

    added prod build for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,29 @@
+var webpack = require('webpack');
+
+var PROD = JSON.parse(process.env.PROD_ENV || '0');
+
+var plugins = [];
+
+if(PROD){
+    plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: { warnings: false }
+        })
+    );
+    plugins.push(
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    );
+}
+
 module.exports = {
     entry: './entry.jsx',
     output: {
-        path: "./build",
-        publicPath: '/build/',
+        path: PROD?"./dist":"./build",
+        publicPath: '/dist/',
         filename: 'bundle.js'
     },
     module: {
@@ -13,5 +34,6 @@ module.exports = {
                 exclude: /node_modules/
             }
         ]
-    }
+    },
+    plugins: plugins
 };
