0a5f3dcfcb69a19efacce8eee461b4d1d4ebb0b8
SuhairZain <suhairzain11@gmail.com>
2016-05-11 23:39:17 +0530

    added empty and unstyled text from TextField

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 import React, {Component} from 'react';
 
-import {Card, CardText} from 'material-ui/Card';
+import {Card} from 'material-ui/Card';
+import TextField from 'material-ui/TextField'
 
 class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            text: ""
+        }
+    }
+
+    onTextChange = (event) => {
+        this.setState({
+            text: event.target.value
+        });
+    };
+
     render() {
         return (
-            <Card>
-                <CardText>Hello MUI</CardText>
-            </Card>
+            <div>
+                <Card>
+                    <TextField
+                        hintText="Enter something here to show it out"
+                        onChange={this.onTextChange}
+                        fullWidth={true}/>
+                </Card>
+                <p>{this.state.text.isEmpty()?"EMPTY":this.state.text}</p>
+            </div>
         );
     }
 }
